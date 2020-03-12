ca3677a1ef9a33f5d9c65885cddd43e01c1e922f
sunny <2453709145@qq.com>
2020-03-12 23:27:04 +0800

    实现关键数字实时更新

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import flask
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import jsonify
 import utils
+
 
 app = Flask(__name__)
 
@@ -14,6 +17,12 @@ def hello_world():
 @app.route("/time")
 def get_time():
     return utils.get_time()
+
+
+@app.route("/centerA")
+def get_centerA_data():
+    data = utils.get_centerA_data()
+    return jsonify({"confirm": int(data[0]),"suspect": int(data[1]),"heal": int(data[2]),"dead": int(data[3])})
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -6,5 +6,48 @@ def get_time():
     return time_str.format("年","月","日")
 
 
+
+def get_conn():
+    # :return:连接，游标
+    # 建立连接
+    conn = pymysql.connect(host="localhost",
+                           user="root",
+                           password="123456",
+                           db="epidemic",
+                           charset="utf8")
+    # 创建游标
+    cursor = conn.cursor()  # 执行完毕返回的结果默认以元组显示
+    return conn,cursor
+
+
+def close_conn(conn, cursor):
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
+
+
+def query(sql,*args):
+    # 封装通用查询，返回查询结果
+    conn,cursor = get_conn()
+    cursor.execute(sql,args)
+    res = cursor.fetchall()
+    close_conn(conn,cursor)
+    return res
+
+
+def get_centerA_data():
+    # 获取centerA实时数据
+    # 取时间戳最新的那组数据
+    sql = "select sum(confirm)," \
+          "(select suspect from history order by ds desc limit 1)," \
+          "sum(heal)," \
+          "sum(dead) " \
+          "from details " \
+          "where update_time=(select update_time from details order by update_time desc limit 1) "
+    res = query(sql)
+    return res[0]
+
+
 if __name__ == "__main__":
-    print(get_time())
+    print(get_centerA_data())
